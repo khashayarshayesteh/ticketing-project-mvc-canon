bb584ac1b7c7071dc4ec2f557f40309b3b4aa0a4
khashayar shayesteh <shayesteh.office@gmail.com>
2022-09-29 07:45:34 -0700

    Initial commit

---
+++ src/main/java/com/canon/TicketingProjectMvcCanonApplication.java
@@ -0,0 +1,13 @@
+package com.canon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectMvcCanonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectMvcCanonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/canon/TicketingProjectMvcCanonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectMvcCanonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
